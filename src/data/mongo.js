b9d42b57ad221b06f0a4876a1880a488554647c0
Đinh Văn Khiêm <dinhvankhiem@Codey.local>
2021-10-10 03:04:01 +0700

    change db connect

--- src/data/mongo.js
+++ src/data/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URL,{
+mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify:true
